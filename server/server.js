ce13911ac2f24d9420451b45ffd6b01c0b0cd42a
anasred <anasahmar@gmail.com>
2017-10-28 19:36:59 +0300

    added socket.io on front and back ends

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 // this is for deploying on heroku
 const port = process.env.PORT || 50001;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => { 
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('socket disconnected');
+    });
+});
+
+server.listen(port, () => { 
     console.log(`server starts at port ${port}`)
 });
